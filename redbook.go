18d0f7a52e07c6fe6525a3a389631c0b62b5407f
MigueLesPaul <miguelhinojosa994@gmail.com>
2025-11-19 13:53:02 +0100

    Test yaml

--- redbook.go
+++ redbook.go
@@ -1,9 +1,14 @@
 package main 
 
-import "redbook/obsidianfrontmatter"
+import (
+	obf "redbook/obsidianfrontmatter"
+	fmt "fmt"
+)
 
 func main(){
+	test_path := "/data/data/com.termux/files/home/storage/shared/Obsidian/Journal/2025-11-19.md"
 
+fmt.Println(obf.ReadFrontMatter(test_path))
 
 
 }
