cb24ca0dc88b52c99abcd4792da77fb387ea448c
MigueLEsPaul <miguelhinojosa994@gmail.com>
2025-11-21 20:22:04 +0100

    FIX: filtering

--- pkg/services/frontmatterstats/frontmatterstats.go
+++ pkg/services/frontmatterstats/frontmatterstats.go
@@ -4,7 +4,13 @@ import (
 	fmt "fmt"
 )
 
-func GetVariableNdayValues(frontmatterList []map[string]interface{}, variable string, ndays int) ([]float64, error) {
+// Fieldfilters for second level maps filters
+type Fieldfilters struct {
+	Field string
+	Value string
+}
+
+func GetVariableNdayValues(frontmatterList []map[string]interface{}, variable string, ndays int, filter Fieldfilters) ([]float64, error) {
 	// Provides a list of n values that represents the daily balance for a specific variable from the frontmatter.
 	// The variables have to be of object(map) type in the yaml
 	// Asumes the list of Daily maps are sorted cronologically
@@ -17,29 +23,31 @@ func GetVariableNdayValues(frontmatterList []map[string]interface{}, variable st
 				balances = append(balances, float64(v))
 			case []interface{}:
 				var balanceHoy float64 = 0.0
+				var value float64 = 0.0
 				for _, sp := range v {
-					// balanceHoy+=sp["value"].(float32)
-					// fmt.Printf(": %T\n", sp)
 					m, ok := sp.(map[string]interface{})
 					if ok {
-						// fmt.Println(m["amount"])
 						if m["value"] != nil {
-							value, ok := m["value"].(float64)
+							value, ok = m["value"].(float64)
 							if !ok {
-								fmt.Println(ok)
+								if i, ok := m["value"].(int); ok {
+									value = float64(i)
+								}
 							}
-							balanceHoy += value
 						}
 						if m["amount"] != nil {
-							value, ok := m["amount"].(float64)
+							value, ok = m["amount"].(float64)
 							if !ok {
 								if i, ok := m["amount"].(int); ok {
 									value = float64(i)
 								}
 							}
-							// fmt.Println(value, ok)
+						}
+
+						if filter.Field == "" || m[filter.Field] == filter.Value {
 							balanceHoy += value
 						}
+
 					}
 				}
 				balances = append(balances, balanceHoy)
@@ -52,10 +60,8 @@ func GetVariableNdayValues(frontmatterList []map[string]interface{}, variable st
 func GetVariableNdayTotal(list []float64) (float64, error) {
 	// Get the total from a list. basically df.sum() :)
 	var sum float64
-
 	for _, v := range list {
 		sum += v
 	}
-
 	return sum, nil
 }
--- redbook.go
+++ redbook.go
@@ -12,22 +12,23 @@ func main() {
 	test_path := "/home/miguel/Documents/Obsidian/Journal"
 	dailyNotes, error := obf.LoadFrontMattersFromDir(test_path)
 	variable := "mood"
-	dailyBalances, error := fmtstats.GetVariableNdayValues(dailyNotes, variable, 30)
+	filter := fmtstats.Fieldfilters{}
+	dailyBalances, error := fmtstats.GetVariableNdayValues(dailyNotes, variable, 30, filter)
 	total, error := fmtstats.GetVariableNdayTotal(dailyBalances)
 
 	if error != nil {
 		panic("Panic at the Dico!")
 	}
 	fmt.Printf("Total de '%s'\n", variable)
-	fmt.Println(dailyBalances)
+	// fmt.Println(dailyBalances)
 	fmt.Println(total)
 
 	variable = "finances"
-
-	dailyBalances, error = fmtstats.GetVariableNdayValues(dailyNotes, variable, 30)
+	filter = fmtstats.Fieldfilters{Field: "currency", Value: "EUR"}
+	dailyBalances, error = fmtstats.GetVariableNdayValues(dailyNotes, variable, 30, filter)
 	total, error = fmtstats.GetVariableNdayTotal(dailyBalances)
 
 	fmt.Printf("Total de '%s'\n", variable)
-	fmt.Println(dailyBalances)
+	// fmt.Println(dailyBalances)
 	fmt.Println(total)
 }
